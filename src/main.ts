97c92ef47e8ab7a87c09605dd11109348a256f55
Cesar BR <cesar@basterisreyes.com>
2025-02-28 15:06:41 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+    'http://localhost:5173',
+    'https://frontend-ld6g9ctk2-cesarbr20s-projects.vercel.app/',
+  ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
