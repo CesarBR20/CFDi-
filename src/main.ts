f80582c56240ecc933f3639f1093ba18440d4c5b
Cesar BR <cesar@basterisreyes.com>
2025-02-28 15:16:07 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,14 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-    'http://localhost:5173',
-    'https://frontend-ld6g9ctk2-cesarbr20s-projects.vercel.app/',
-  ],
+      'http://localhost:5173',
+      'https://frontend-ld6g9ctk2-cesarbr20s-projects.vercel.app'
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
